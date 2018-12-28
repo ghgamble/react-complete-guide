395c193eb13b56ef857a4daae51724ee6a952df6
Grace Gamble <grace.h.gamble@gmail.com>
2018-12-28 14:47:29 -0500

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -10,12 +10,12 @@ class App extends Component {
                   { name: 'Eloise', age: 61}
             ]
       }
-      switchNameHandler = () => {
+      switchNameHandler = (newName) => {
             // console.log('Was clicked');
             // DON'T DO THIS: this.state.persons[0].name = 'Grace G';
             this.setState({
                   persons: [
-                        { name: 'Grace G', age: 34 },
+                        { name: newName, age: 34 },
                         { name: 'Eric', age: 47 },
                         { name: 'Eloise', age: 61 }
                   ]
@@ -26,10 +26,17 @@ class App extends Component {
                   <div className="App">
                         <h1>Hi, I'm a React App</h1>
                         <p>This is really working!</p>
-                        <button onClick={this.switchNameHandler}>Switch Name</button>
-                        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Magic</Person>
-                        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                        <button onClick={() => this.switchNameHandler('Grace G')}>Switch Name</button>
+                        <Person
+                              name={this.state.persons[0].name}
+                              age={this.state.persons[0].age} />
+                        <Person
+                              name={this.state.persons[1].name}
+                              age={this.state.persons[1].age}
+                              click={this.switchNameHandler.bind(this, 'Eric!')}> My Hobbies: Magic</Person>
+                        <Person
+                              name={this.state.persons[2].name}
+                              age={this.state.persons[2].age} />
                   </div>
             );
             // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
