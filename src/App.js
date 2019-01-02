22d3a93d2d530bb431c719bd602ec5e620fd1573
Grace Gamble <grace.h.gamble@gmail.com>
2019-01-02 10:35:22 -0700

    Handling Dynamic Content the 'JavaScript Way'

--- src/App.js
+++ src/App.js
@@ -44,32 +44,34 @@ class App extends Component {
                   padding: '8px',
                   cursor: 'pointer'
             };
+            let persons = null;
+            if (this.state.showPersons) {
+                  persons = (
+                        <div>
+                              <Person
+                                    name={this.state.persons[0].name}
+                                    age={this.state.persons[0].age} />
+                              <Person
+                                    name={this.state.persons[1].name}
+                                    age={this.state.persons[1].age}
+                                    click={this.switchNameHandler.bind(this, 'Eric!')}
+                                    changed={this.nameChangedHandler}>
+                                    My Hobbies: Magic</Person>
+                              <Person
+                                    name={this.state.persons[2].name}
+                                    age={this.state.persons[2].age} />
+                        </div>
+                  );
+            }
             return (
                   <div className="App">
                         <h1>Hi, I'm a React App</h1>
                         <p>This is really working!</p>
                         <button
                               style={style}
-                              onClick={this.togglePersonsHandler}>Switch Name
+                              onClick={this.togglePersonsHandler}>Toggle Persons
                         </button>
-                        {
-                              this.state.showPersons === true ?
-                              <div>
-                                    <Person
-                                          name={this.state.persons[0].name}
-                                          age={this.state.persons[0].age} />
-                                    <Person
-                                          name={this.state.persons[1].name}
-                                          age={this.state.persons[1].age}
-                                          click={this.switchNameHandler.bind(this, 'Eric!')}
-                                          changed={this.nameChangedHandler}>
-                                          My Hobbies: Magic</Person>
-                                    <Person
-                                          name={this.state.persons[2].name}
-                                          age={this.state.persons[2].age} />
-
-                              </div> : null
-                        }
+                        {persons}
                   </div>
             );
       }
