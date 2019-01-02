b1c02b4a01196af5ba9f5d054168fbe8995a2a44
Grace Gamble <grace.h.gamble@gmail.com>
2019-01-02 11:28:14 -0700

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -18,14 +18,18 @@ class App extends Component {
             persons.splice(personIndex, 1);
             this.setState({persons: persons});
       }
-      nameChangedHandler = (event) => {
-            this.setState({
-                  persons: [
-                        { name: 'Grace', age: 34 },
-                        { name: event.target.value, age: 47 },
-                        { name: 'Eloise', age: 61 }
-                  ]
-            })
+      nameChangedHandler = (event, id) => {
+            const personIndex = this.state.persons.findIndex(p => {
+                  return p.id === id;
+            });
+            const person = {
+                  ...this.state.persons[personIndex]
+            };
+            // const person = Object.assign({}, this.state.persons[personIndex]);
+            person.name = event.target.value;
+            const persons = [...this.state.persons];
+            persons[personIndex] = person;
+            this.setState({persons: persons})
       }
       togglePersonsHandler = () => {
             const doesShow = this.state.showPersons;
@@ -48,7 +52,8 @@ class App extends Component {
                                           click={() => this.deletePersonHandler(index)}
                                           name={person.name}
                                           age={person.age}
-                                          key={person.id} />
+                                          key={person.id}
+                                          changed={(event) => this.nameChangedHandler(event, person.id)} />
                               })}
                         </div>
                   );
