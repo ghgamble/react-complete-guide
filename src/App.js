c60c32663f12dc69475f68a62957a71e6bb71141
Grace Gamble <grace.h.gamble@gmail.com>
2019-01-03 16:21:49 -0700

    Remove Radium References

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
       state = {
@@ -42,11 +41,7 @@ class App extends Component {
                   font: 'inherit',
                   border: '1px solid blue',
                   padding: '8px',
-                  cursor: 'pointer',
-                  ':hover': {
-                        backgroundColor: 'lightgreen',
-                        color: 'black'
-                  }
+                  cursor: 'pointer'
             };
             let persons = null;
             if (this.state.showPersons) {
@@ -78,20 +73,18 @@ class App extends Component {
             }
 
             return (
-                  <StyleRoot>
-                        <div className="App">
-                              <h1>Hi, I'm a React App</h1>
-                              <p className={classes.join(' ')}>This is really working!</p>
-                              <button
-                                    style={style}
-                                    onClick={this.togglePersonsHandler}>
-                                    Toggle Persons
-                              </button>
-                              {persons}
-                        </div>
-                  </StyleRoot>
+                  <div className="App">
+                        <h1>Hi, I'm a React App</h1>
+                        <p className={classes.join(' ')}>This is really working!</p>
+                        <button
+                              style={style}
+                              onClick={this.togglePersonsHandler}>
+                              Toggle Persons
+                        </button>
+                        {persons}
+                  </div>
             );
       }
 }
 
-export default Radium(App);
+export default App;
