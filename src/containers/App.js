d0013b3d559534f1bbb1cf97c346148f64fbade4
Grace Gamble <grace.h.gamble@gmail.com>
2019-01-07 15:29:43 -0700

    Splitting An App Into Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
       state = {
@@ -36,43 +37,22 @@ class App extends Component {
       }
       render() {
             let persons = null;
-            let btnClass = '';
             if (this.state.showPersons) {
-                  persons = (
-                        <div>
-                              {this.state.persons.map((person, index) => {
-                                    return (
-                                    <Person
-                                          click={() => this.deletePersonHandler(index)}
-                                          key={person.id}
-                                          name={person.name}
-                                          age={person.age}
-                                          changed={(event) => this.nameChangedHandler(event, person.id)}
-                                          />
-                                    )
-                              })}
-                        </div>
-                  );
-                  btnClass = classes.Red;
-            }
-
-            const assignedClasses = []; // red bold
-            if (this.state.persons.length <= 2) {
-                  assignedClasses.push(classes.red);
-            }
-            if (this.state.persons.length <= 1) {
-                  assignedClasses.push(classes.bold);
+                  persons =
+                        <Persons
+                              persons={this.state.persons}
+                              clicked={this.deletePersonHandler}
+                              changed={this.nameChangedHandler}
+                        />
             }
 
             return (
                   <div className={classes.App}>
-                        <h1>Hi, I'm a React App</h1>
-                        <p className={assignedClasses.join(' ')}>This is really working!</p>
-                        <button
-                              className={btnClass}
-                              onClick={this.togglePersonsHandler}>
-                              Toggle Persons
-                        </button>
+                        <Cockpit
+                              showPersons={this.state.showPersons}
+                              persons={this.state.persons}
+                              clicked={this.togglePersonsHandler}
+                        />
                         {persons}
                   </div>
             );
