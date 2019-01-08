aa37787729936410e16c158fc5d1e678365bbad1
Grace Gamble <grace.h.gamble@gmail.com>
2019-01-08 09:26:53 -0700

    Using setState Correctly

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,8 @@ class App extends PureComponent {
                         { id: 'aple', name: 'Eric', age: 46},
                         { id: 'bioe', name: 'Eloise', age: 61}
                   ],
-                  showPersons: false
+                  showPersons: false,
+                  toggleClicked: 0
             }
       }
       componentWillMount() {
@@ -42,7 +43,8 @@ class App extends PureComponent {
                   { id: 'aple', name: 'Eric', age: 46},
                   { id: 'bioe', name: 'Eloise', age: 61}
             ],
-            showPersons: false
+            showPersons: false,
+            toggleClicked: 0
       }
       deletePersonHandler = (personIndex) => {
             // const persons = this.state.persons.slice();
@@ -65,7 +67,12 @@ class App extends PureComponent {
       }
       togglePersonsHandler = () => {
             const doesShow = this.state.showPersons;
-            this.setState({showPersons: !doesShow});
+            this.setState((prevState, props) => {
+                  return {
+                        showPersons: !doesShow,
+                        toggleClicked: prevState.toggleClicked + 1
+                  }
+            });
       }
       render() {
             console.log('[App.js] inside Render');
